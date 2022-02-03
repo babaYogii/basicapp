863518697f31480990db75537e226d0955db4989
Yogesh Prabhakar Kodlinge <yogeshkodlinge@gmail.com>
2022-02-03 23:08:35 +0530

    New Component added form Differnet Branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import First from './Component/First';
+import NewComp from './Component/NewComp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NewComp/>
       <First/>
        My first Commits on Git
       </header>
