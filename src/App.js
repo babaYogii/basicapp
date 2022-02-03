2faeaf05e5f905a8ff4bd7ef9d65af9b9be541ff
Yogesh Prabhakar Kodlinge <yogeshkodlinge@gmail.com>
2022-02-03 22:52:18 +0530

    Heading added Welcome React With Different Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import First from './Component/First';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <First/>
        My first Commits on Git
       </header>
     </div>
